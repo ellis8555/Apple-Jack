5c98b72423e1ccc95077caafeeeb6c8397d73e0b
ellis <lsp1@hotmail.com>
2022-01-13 07:51:49 -0800

    edited branch second

--- test.js
+++ test.js
@@ -4,10 +4,8 @@
 // The above line was edited in branch six and used to say,
 // "I will edit this line in the fifth branch!"
 
-function myTest(six) {
-  console.log(six);
+function myTest(second) {
+  console.log(second);
   // This is my first test
-  console.log(
-    "This is newly added in branch master before being merged with first."
-  );
+  console.log("This message has been edited in branch second");
 }
