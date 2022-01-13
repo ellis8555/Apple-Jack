20edd72308d5588671fa1cff8c34a735ef4e2d89
ellis <lsp1@hotmail.com>
2022-01-13 07:40:42 -0800

    another test commit

---
+++ test.js
@@ -0,0 +1,10 @@
+// This line edited in branch four! Now its been edited in six.
+
+// Now I'm in VS Code!
+// The above line was edited in branch six and used to say,
+// "I will edit this line in the fifth branch!"
+
+function myTest(six) {
+  console.log(six);
+  // This is my first test
+}
