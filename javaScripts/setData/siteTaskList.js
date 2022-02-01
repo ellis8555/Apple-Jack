64358efab9c2eedc98947fa352b3b2fcc8bef89d
ellis <lsp1@hotmail.com>
2022-02-01 15:46:46 -0800

    edit site page message

--- javaScripts/setData/siteTaskList.js
+++ javaScripts/setData/siteTaskList.js
@@ -6,17 +6,10 @@ export function displayTaskList() {
 
   displayArea.innerHTML = `<div class="w3-content w3-margin w3-padding w3-round-large w3-display-container w3-margin-top w3-card-4 w3-text-black">
   <h3>Message</h3>  
-  <p>
-      This page I feel is functional enough for myself to update and log games
-      for a new season. I think it's functional enough to handle multiple
-      seasons with new data entries made. I hope new players and or teams won't
-      break the site! That will be embarrassing! I was going to wait for further
-      testing but my patience has run out with myself so I just wanna get this
-      up!
-    </p>
+  
     <p>The player stats that have been logged have been changed as in a games replay there are different stats that are recorded by the game. For instance the game does not have "own goals" or "saves". Also when totals rang up for a teams total goals for the season these numbers did not match the total goals from the players on that team. This is where I evenly distributed missing tallies.</p>
     <h3>List of site improvements</h3>
-    <p>1. Team icons to display team data. Game results first</p>
+    <p>1. Improve team data page</p>
     <p>2. Other team related stats</p>
     <p>In no particular order..</p>
     <p>Improve the little logo for the site. Used in bookmarks/tabs.</p>
