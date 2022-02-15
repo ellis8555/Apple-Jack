f7efba75bc9fed3146c3c942bc12eee5c9f96f1b
ellis <lsp1@hotmail.com>
2022-02-15 08:34:30 -0800

    edit site message page

--- javaScripts/setData/siteTaskList.js
+++ javaScripts/setData/siteTaskList.js
@@ -10,13 +10,11 @@ export function displayTaskList() {
   displayArea.innerHTML = `<div class="w3-content w3-margin w3-padding w3-round-large w3-display-container w3-margin-top w3-card-4 w3-text-black">
   <p><b>*</b> HaxBall main logo is a link back to home settings of the page. <b>*</b></p>
   <p><b>**</b> Tables are sortable via clicking the field header. On mobile click on any row <b>**</b></p>
+  <p><b>***</b> Team pages player stats are NOT sortable at this point <b>***</b></p>
  
     <h3>List of site improvements</h3>
-    <p>1. Improve team data page.</p>
-    <p>2. Gif highlights. Somewhere located near a game result have access to that games giffy highlights.</p>
+    <p>1. Improve teams game results page. Add logos/game stats/gifs</p> 
     <h3>In no particular order..</h3>
-    <p>Playoffs page of some sort.</p>
-    <p>For each game result have all that games data appear if clicked on.</p>
     <p>Sidebar improvement</p>
     <p>Improve the little logo for the site. Used in bookmarks/tabs.</p>
     <p>
