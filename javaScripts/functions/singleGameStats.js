2353f3fa307000ba1e6a3bba3a3f925cb972f7c3
ellis <lsp1@hotmail.com>
2022-02-23 15:29:10 -0800

    rough in of boxscores being displayed

--- javaScripts/functions/singleGameStats.js
+++ javaScripts/functions/singleGameStats.js
@@ -8,56 +8,98 @@ import {
   clearScoreboardDiv,
   clearTablesDiv,
   getScoreboardDiv,
+  getTablesDiv,
 } from "./variousFunctions.js";
 
 export default function setGamesData(e) {
   clearScoreboardDiv();
   clearTablesDiv();
   getScoreboardDiv();
+  getTablesDiv();
   let displayGameData = "";
+  let displayTeamLogos = "";
   let gameNumber = e.target.dataset.gameId;
   let thisSeasonNumber = gameResults.filter(
     (item) => item.GameID == gameNumber
   )[0].SeasonNumber;
   let thisGamesResult = gameResults.filter((item) => item.GameID == gameNumber);
+  // begin home team
   let thisGamesHomeTeam = teamsMAP.get(+thisGamesResult[0].TeamOne);
   let thisGamesHomeTeamLogo =
     eachTeamObjectMAP.get(thisGamesHomeTeam)[
       `S0${thisSeasonNumber}HomeFilePath`
     ];
   let thisGamesHomeTeamScore = thisGamesResult[0].TeamOneScore;
+  let thisGamesHomeTeamPossession = thisGamesResult[0].TeamOnePossession;
+  let thisGamesHomeTeamShotsOnGoal = thisGamesResult[0].TeamOneShotsOnGoal;
+  let thisGamesHomeTeamPasses = thisGamesResult[0].TeamOnePasses;
+  let thisGamesHomeTeamKicks = thisGamesResult[0].TeamOneKicks;
+  // end home team
+  // begin away team
   let thisGamesAwayTeam = teamsMAP.get(+thisGamesResult[0].TeamTwo);
   let thisGamesAwayTeamLogo =
     eachTeamObjectMAP.get(thisGamesAwayTeam)[
       `S0${thisSeasonNumber}HomeFilePath`
     ];
   let thisGamesAwayTeamScore = thisGamesResult[0].TeamTwoScore;
-  let thisGamesHighlights = gifs.filter((item) => item.GameID == gameNumber);
-  if (thisGamesHighlights.length > 0) {
-    // boxscore div container
-    displayGameData += `<div class="boxscoreContainer">`;
-    // begin data containers
-    displayGameData += `<div class="boxscoreHomeTeamLogo">`;
-    displayGameData += `<img src="${thisGamesHomeTeamLogo}">`;
+  let thisGamesAwayTeamPossession = thisGamesResult[0].TeamTwoPossession;
+  let thisGamesAwayTeamShotsOnGoal = thisGamesResult[0].TeamTwoShotsOnGoal;
+  let thisGamesAwayTeamPasses = thisGamesResult[0].TeamTwoPasses;
+  let thisGamesAwayTeamKicks = thisGamesResult[0].TeamTwoKicks;
+  // end away team
+
+  let gameCategories = ["Score", "Possession", "SOG", "Passes", "Kicks"];
+  let thisGamesHomeTeamStats = [
+    thisGamesHomeTeamScore,
+    thisGamesHomeTeamPossession,
+    thisGamesHomeTeamShotsOnGoal,
+    thisGamesHomeTeamPasses,
+    thisGamesHomeTeamKicks,
+  ];
+  let thisGamesAwayTeamStats = [
+    thisGamesAwayTeamScore,
+    thisGamesAwayTeamPossession,
+    thisGamesAwayTeamShotsOnGoal,
+    thisGamesAwayTeamPasses,
+    thisGamesAwayTeamKicks,
+  ];
+  // boxscore div container
+  displayGameData += `<div class="boxscoreContainer">`;
+  // begin data containers
+  // displayGameData += `<div class="boxscoreHomeTeamLogo">`;
+  // displayGameData += `<img src="${thisGamesHomeTeamLogo}">`;
+  // displayGameData += `</div>`;
+  for (let i = 0; i < gameCategories.length; i++) {
+    displayGameData += `<div class="boxscoreHomeStats">`;
+    displayGameData += `${thisGamesHomeTeamStats[i]}`;
     displayGameData += `</div>`;
-    displayGameData += `<div class="boxscoreHomeTeamStats">`;
-    displayGameData += `${thisGamesHomeTeamScore}`;
-    displayGameData += `</div>`;
+
     displayGameData += `<div class="boxscoreSeperator">`;
-    displayGameData += ` : `;
+    displayGameData += `${gameCategories[i]}`;
+
     displayGameData += `</div>`;
+
     displayGameData += `<div class="boxscoreAwayStats">`;
-    displayGameData += `${thisGamesAwayTeamScore}`;
+    displayGameData += `${thisGamesAwayTeamStats[i]}`;
     displayGameData += `</div>`;
-    displayGameData += `<div class="boxscoreAwayLogo">`;
-    displayGameData += `<img src="${thisGamesAwayTeamLogo}">`;
-    displayGameData += `</div>`;
-    //end data containers
-    // end boxscore container div
-    displayGameData += `</div>`;
-    scoreboardDiv.innerHTML = displayGameData;
-  } else {
-    displayGameData = "<h3>No highlights for this game</h3>";
-    scoreboardDiv.innerHTML = displayGameData;
   }
+
+  // displayGameData += `<div class="boxscoreAwayLogo">`;
+  // displayGameData += `<img src="${thisGamesAwayTeamLogo}">`;
+  // displayGameData += `</div>`;
+  //end data containers
+  // end boxscore container div
+  displayGameData += `</div>`;
+  // team logos
+  displayTeamLogos += `<div class="boxscoreTeamLogosContainer">`;
+  displayTeamLogos += `<div class="boxscoreHomeTeamLogo">`;
+  displayTeamLogos += `<img src="${thisGamesHomeTeamLogo}">`;
+  displayTeamLogos += `</div>`;
+  displayTeamLogos += `<div class="boxscoreAwayTeamLogo">`;
+  displayTeamLogos += `<img src="${thisGamesAwayTeamLogo}">`;
+  displayTeamLogos += `</div>`;
+  displayTeamLogos += `</div>`;
+
+  tablesDiv.innerHTML = displayTeamLogos;
+  scoreboardDiv.innerHTML = displayGameData;
 }
