6befb38ffc07a949476cdc5a1b093381ab887c48
ellis <lsp1@hotmail.com>
2022-03-26 18:30:49 -0700

    playersBg colors working in all tables

--- javaScripts/functions/playerTableBgColor.js
+++ javaScripts/functions/playerTableBgColor.js
@@ -26,6 +26,23 @@ export default function setPlayersTableBgColor(seasonNumber = currentSeason) {
       item.style.color = "white";
     });
   } else {
-    console.log("test");
+    playerRow.forEach((item) => {
+      let playersName = item.textContent;
+      let playersID = playersNumMAP.get(playersName);
+      let playersTeams = teamPlayers.filter(
+        (item) => item.PlayerID == playersID
+      );
+      playersTeams.sort((a, b) => a.SeasonNumber - b.SeasonNumber);
+      let getPlayersMostRecentSeasonsRecord =
+        playersTeams[playersTeams.length - 1];
+      if (+getPlayersMostRecentSeasonsRecord.SeasonNumber == currentSeason) {
+        let playersMostRecentTeam = getPlayersMostRecentSeasonsRecord.TeamID;
+        let playersTeamName = teamsMAP.get(+playersMostRecentTeam);
+        item.style.backgroundColor = `#${
+          eachTeamObjectMAP.get(playersTeamName).MainColor
+        }`;
+        item.style.color = "white";
+      }
+    });
   }
 }
