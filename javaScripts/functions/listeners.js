f0cd06b46ba27ea48a9ae4d71034768e6876a703
ellis <lsp1@hotmail.com>
2022-08-05 19:47:21 -0600

    listeners.js setTableListeners() onclick funtion added/addeventlisteners removed

--- javaScripts/functions/listeners.js
+++ javaScripts/functions/listeners.js
@@ -36,13 +36,19 @@ import screenResize from "./resize.js";
 export function setTableListeners() {
   let browserWidth = window.innerWidth;
   if (browserWidth < 982) {
-    let mobileTableCells = document.querySelectorAll("#tablesDiv td");
-    let eachCell = Array.from(mobileTableCells);
-    eachCell.forEach((field) => field.addEventListener("click", sortTable));
+    let mobileTableCells = document.querySelectorAll("#tablesDiv");
+    mobileTableCells[0].onclick = function (e) {
+      let td = e.target.closest("td");
+      if (!td) return;
+      sortTable(e);
+    };
   } else {
-    let getFieldNames = document.querySelectorAll("#tablesDiv th");
-    let headers = Array.from(getFieldNames);
-    headers.forEach((field) => field.addEventListener("click", sortTable));
+    let getFieldNames = document.querySelectorAll("#tablesDiv");
+    getFieldNames[0].onclick = function (e) {
+      let th = e.target.closest("th");
+      if (!th) return;
+      sortTable(e);
+    };
   }
 }
 setTableListeners();
