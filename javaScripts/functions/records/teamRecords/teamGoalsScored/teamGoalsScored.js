7bf8aa9eeb11eac7523b246d25124b2d07e57483
ellis <lsp1@hotmail.com>
2022-09-11 10:43:20 -0600

    most team goals in progress

--- javaScripts/functions/records/teamRecords/teamGoalsScored/teamGoalsScored.js
+++ javaScripts/functions/records/teamRecords/teamGoalsScored/teamGoalsScored.js
@@ -6,71 +6,54 @@ import {
   teamPlayers,
   playersMAP,
 } from "../../../../../json/masterVars.js";
-import {
-  closeSidebar,
-  clearScoreboardDiv,
-  getScoreboardDiv,
-  clearTablesDiv,
-} from "../../../variousFunctions.js";
+import sortGroupedStats from "../../../sort.js";
 
-export function getMostGoalsScoredByATeam() {
-  clearTablesDiv();
-  clearScoreboardDiv();
-  closeSidebar();
-  getScoreboardDiv();
-  const highestHomeTeamScore = Math.max(
-    ...Array.from(new Set(gameResults.map((score) => score.TeamOneScore)))
-  );
-  const highestAwayTeamScore = Math.max(
-    ...Array.from(new Set(gameResults.map((score) => score.TeamTwoScore)))
-  );
-  const allTimeMostGoalsScoredByTeam = Math.max(
-    ...Array.from(new Set([highestHomeTeamScore, highestAwayTeamScore]))
-  ).toString();
+console.log(gameResults);
+export function getMostGoalsScoredByATeam(seasonMode) {
+  const goalsContainingArr = [];
+  gameResults.forEach((game) => {
+    const gameID = +game.GameID;
+    const homeGoals = +game.TeamOneScore;
+    const awayGoals = +game.TeamTwoScore;
+    const thisGamesData = {};
+    thisGamesData.gameID = gameID;
 
-  function getResult(game) {
-    if (game.TeamOneScore == allTimeMostGoalsScoredByTeam) {
-      return true;
+    if (homeGoals > awayGoals) {
+      thisGamesData.whichSide = "TeamOneScore";
+      thisGamesData.goals = homeGoals;
+      goalsContainingArr.push(thisGamesData);
+    } else if (homeGoals < awayGoals) {
+      thisGamesData.whichSide = "TeamTwoScore";
+      thisGamesData.goals = awayGoals;
+      goalsContainingArr.push(thisGamesData);
+    } else {
+      thisGamesData.whichSide = "TeamOneScore";
+      thisGamesData.goals = homeGoals;
+      goalsContainingArr.push(thisGamesData);
     }
-    if (game.TeamTwoScore == allTimeMostGoalsScoredByTeam) {
-      return true;
-    }
-  }
-
-  const getGameRecords = gameResults.filter(getResult);
-
-  let recordHolder;
-
-  if (getGameRecords[0].TeamOneScore == allTimeMostGoalsScoredByTeam) {
-    recordHolder = teamsMAP.get(Number(getGameRecords[0].TeamOne));
-  } else {
-    recordHolder = teamsMAP.get(Number(getGameRecords[0].TeamTwo));
-  }
-
-  function identifyTeamMembers(player) {
-    if (
-      player.TeamID == teamsNumMAP.get(recordHolder) &&
-      player.SeasonNumber == getGameRecords[0].SeasonNumber
-    ) {
-      return true;
-    }
-  }
-
-  const getTeamMembers = teamPlayers.filter(identifyTeamMembers);
-
-  let data = `<div class="w3-red w3-padding w3-round">Under contstruction</div><br>`;
-  data += `<div style="text-align:left;" class="w3-blue w3-round w3-padding w3-card-4">`;
-  data += `Most goals scored in a game by a team: <b>${allTimeMostGoalsScoredByTeam}</b><br>`;
-  data += `Team: <span class="w3-round"style="background-color: #${
-    eachTeamObjectMAP.get(recordHolder).MainColor
-  }">${recordHolder}</span><br>`;
-  data += ` Members: `;
-  getTeamMembers.forEach((player) => {
-    data += `${playersMAP.get(Number(player.PlayerID))}, `;
   });
-  data += `<br>`;
-  data += `Season: ${getGameRecords[0].SeasonNumber}`;
-  data += `</div>`;
 
-  scoreboardDiv.innerHTML = data;
+  const getEachUniqueGoalCount = new Set();
+  goalsContainingArr.forEach((getGoals) => {
+    getEachUniqueGoalCount.add(getGoals.goals);
+  });
+  const sortEachUniqueGoalCount = [...getEachUniqueGoalCount].sort((a, b) => {
+    return b - a;
+  });
+
+  const getTopThreeGoalsGameID = goalsContainingArr.filter((game) => {
+    return game.goals >= sortEachUniqueGoalCount[2];
+  });
+
+  const getTopThreeGoalsScoredGameResults = [];
+  getTopThreeGoalsGameID.forEach((getGame) => {
+    gameResults.forEach((findGameID) => {
+      if (findGameID.GameID == getGame.gameID) {
+        getTopThreeGoalsScoredGameResults.push(findGameID);
+      }
+    });
+  });
+
+  // NEED TO EXTRACT WHICH SIDE IS THE HIGHER COUNT AND ALSO FILTER FROM SEASON MODE
+  console.log(getTopThreeGoalsScoredGameResults);
 }
