2cfe31e17fc1a0b89bd7dd4f7bbd0d8f1adf7e01
ellis <lsp1@hotmail.com>
2022-09-12 13:27:30 -0600

    announcement adjusted season 3 and league records framework

--- javaScripts/setData/announcements.js
+++ javaScripts/setData/announcements.js
@@ -8,8 +8,8 @@ export function displayTaskList() {
   let displayArea = document.getElementById("tablesDiv");
 
   displayArea.innerHTML = `<div class="w3-content w3-margin w3-padding w3-round-large w3-display-container w3-margin-top w3-card-4 w3-text-black">
-  <div style="margin: auto; width:90%; background-color: rgb(34, 184, 34);"><h5>Season 3 tentative start beginning of September 2022!</h5></div>
-  <p><b>League Records data next project</b></p>
+  <div style="margin: auto; width:90%; background-color: rgb(34, 184, 34);"><h5>Season 3 beginning very soon!</h5></div>
+  <p style="background-color: orange;"><b>A first league record stat has been completed laying out the framework for more stats in the future!</b></p>
   <b>*</b> HaxBall main logo is a link back to home settings of the page. <b>*</b></p>
   <p><b>**</b> Tables are sortable via clicking the field header. On mobile click on any row <b>**</b></p>
   </div>`;
