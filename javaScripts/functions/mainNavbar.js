eb7788ed6c5180e8e0080ae682ae4d03aca7c109
ellis <lsp1@hotmail.com>
2022-09-23 07:50:34 -0600

    removed navbar layout experiment for mobile many teams

--- javaScripts/functions/mainNavbar.js
+++ javaScripts/functions/mainNavbar.js
@@ -7,12 +7,10 @@ import {
   teams,
 } from "../../json/masterVars.js";
 import { setListenersMainNavbar } from "./listeners.js";
-import mainNavbarLayout from "./mainNavbarLayout.js";
 
 export default function setMainNavbar(season = currentSeason) {
   const teamsThisSeason = eachSeasonsTeamsMAP.get(season).length;
   let navbarContainer = document.querySelector("#teamsNavbar > section");
-  mainNavbarLayout(teamsThisSeason, navbarContainer);
   let navbarContent = "";
   for (let i = 0; i < teamsThisSeason; i++) {
     let imageSource = eachTeamObjectMAP.get(eachSeasonsTeamsMAP.get(season)[i])[
--- javaScripts/functions/mainNavbarLayout.js
+++
@@ -1,8 +0,0 @@
-export default function mainNavbarLayout(teamCount, containingElement) {
-  const screenWidth = window.innerWidth;
-  if (screenWidth < 500 && teamCount > 5) {
-    containingElement.style.flexWrap = "wrap";
-  } else {
-    containingElement.style.flexWrap = "";
-  }
-}
