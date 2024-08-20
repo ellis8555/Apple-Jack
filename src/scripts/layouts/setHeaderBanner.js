5ea0b123660d554982ebadd6fb1508e3dd70e453
ellis <lsp1@hotmail.com>
2024-08-19 20:13:58 -0600

    rename argument to teamName

--- src/scripts/layouts/setHeaderBanner.js
+++ src/scripts/layouts/setHeaderBanner.js
@@ -1,15 +1,15 @@
 import { DEFENDING_CHAMPS } from "../../constants/consts/vars";
 
-export default function setHeaderBanner(imgSource, team) {
+export default function setHeaderBanner(imgSource, teamName) {
     // note this is not the champions banner
     let headerImage = document.querySelector("#championsCard>div>img");
     headerImage.src = imgSource;
     let header = document.getElementById("headerTeamName");
     let headerChildren = header.childNodes;
-    headerChildren[headerChildren.length-1].textContent = team;
+    headerChildren[headerChildren.length-1].textContent = teamName;
     // add champions name here to display trophy icon when champion team selected
     let trophy = document.querySelector("i");
-    if (team == DEFENDING_CHAMPS) {
+    if (teamName == DEFENDING_CHAMPS) {
       trophy.classList.add("fa-trophy");
     } else {
       trophy.classList.remove("fa-trophy");
