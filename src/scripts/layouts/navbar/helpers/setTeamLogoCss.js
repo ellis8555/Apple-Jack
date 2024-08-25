a754389013afcd65678721aa79787e91605481b2
ellis <lsp1@hotmail.com>
2024-08-25 08:25:48 -0600

    import getTeams3dColorScheme and HC_FONT

--- src/scripts/layouts/navbar/helpers/setTeamLogoCss.js
+++ src/scripts/layouts/navbar/helpers/setTeamLogoCss.js
@@ -1,5 +1,7 @@
 import eachTeamObjectMAP from "../../../var_lib/maps/teams/eachTeamObjectMAP";
 import eachSeasonsTeamsMAP from "../../../var_lib/maps/teams/eachSeasonsTeamsMAP";
+import getTeams3dColorScheme from "../../../misc/getTeams3DColorScheme"
+import { HC_FONT } from "../../../../constants/consts/vars";
 
 function setTeamLogoCss(element, season, id = undefined, teamNameParam = undefined) {
     let teamName;
@@ -16,23 +18,6 @@ function setTeamLogoCss(element, season, id = undefined, teamNameParam = undefin
     const colorParts = teamsColorScheme.split(" ")
     const mainColor = colorParts[2];
 
-    function getTeams3dColorScheme(){
-        let teams3dColorScheme;
-        const colorPartsLength = colorParts.length;
-        switch(colorPartsLength){
-            case 3:
-                teams3dColorScheme = `#${colorParts[2]}`
-                return teams3dColorScheme
-            case 4:
-                teams3dColorScheme = `linear-gradient(to right, #${colorParts[2]} 50%, #${colorParts[3]} 51%)`
-                return teams3dColorScheme
-            case 5:
-                teams3dColorScheme = `linear-gradient(to right, #${colorParts[2]} 35%, #${colorParts[3]} 36% 64%, #${colorParts[4]} 65%)`
-                return teams3dColorScheme
-                default:
-                return `#${mainColor}`
-        }
-    }
     element += `<div class="w3-container w3-cell w3-cell-middle">`; // begin first div
     element += `<div class="${id != undefined ?'w3-card-4 w3-blue w3-round-xlarge':''} w3-padding-small w3-section">`; // begin second div
     // begin third div
@@ -45,10 +30,10 @@ function setTeamLogoCss(element, season, id = undefined, teamNameParam = undefin
     rgba(255, 255, 255, 0.8) 0%, 
     rgba(0, 0, 0, 0.2) 40%, 
     rgba(0, 0, 0, 0.2) 100%),
-    ${getTeams3dColorScheme()};
+    ${getTeams3dColorScheme(mainColor, colorParts)};
     transform: rotate(${colorParts[0]}deg);"
     >`; 
-    element += `<div style="color: #${colorParts[1]};font-weight: 500;font-size: clamp(1rem, 1.5vw, 6.25rem); transform: rotate(-${colorParts[0]}deg);">HC</div>` // 4th div
+    element += `<div style="color: #${colorParts[1]};font-weight: 500;font-size: ${HC_FONT.setTeamLogoCss}; transform: rotate(-${colorParts[0]}deg);">HC</div>` // 4th div
     element += `</div>`; // end third div
     element += `</div>`; // end second div
     element += `</div>`; // end first div
