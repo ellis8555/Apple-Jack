c7da14d4a12db2668843e7c2056f934795f75e25
ellis <lsp1@hotmail.com>
2024-08-26 14:29:50 -0600

    change announcements.js to use dom methods over +=

--- src/scripts/layouts/announcements.js
+++ src/scripts/layouts/announcements.js
@@ -1,15 +1,51 @@
 import closeSidebar from "../sidebar/closeSidebar.js";
 import clearScoreboardDiv from "../scoreboard/clearScoreboardDiv.js";
+
+export default function announcements() {
+  clearScoreboardDiv();
   
-  export default function announcements() {
-    clearScoreboardDiv();
-    let displayArea = document.getElementById("tablesDiv");
-  
-    displayArea.innerHTML = `<div class="w3-content w3-margin w3-padding w3-round-large w3-display-container w3-margin-top w3-card-4 w3-text-black">
-    <div style="margin: auto; width:90%; background-color: rgb(0,255,0);color: #000000;"><h5><b>Season 4 has begun!</b></h5></div>
-    <p style="background-color: orange;"><b>A tonne of stats yet to be created over time. A playoff page, list of Champions etc...</b></p>
-    <b>*</b> HaxBall main logo is a link back to home settings of the page. <b>*</b></p>
-    <p><b>**</b> Tables are sortable via clicking the field header. On mobile click on any row <b>**</b></p>
-    </div>`;
-    closeSidebar();
-  }
+  // Get the display area where you want to add content
+  let displayArea = document.getElementById("tablesDiv");
+
+  // Clear any existing content in the display area
+  displayArea.innerHTML = '';
+
+  // Create the main container div
+  const containerDiv = document.createElement('div');
+  containerDiv.className = 'w3-content w3-margin w3-padding w3-round-large w3-display-container w3-margin-top w3-card-4 w3-text-black';
+
+  // Create the first inner div
+  const announcementDiv = document.createElement('div');
+  announcementDiv.style.margin = 'auto';
+  announcementDiv.style.width = '90%';
+  announcementDiv.style.backgroundColor = 'rgb(0,255,0)';
+  announcementDiv.style.color = '#000000';
+
+  // Create the heading element inside the first inner div
+  const heading = document.createElement('h5');
+  heading.innerHTML = '<b>Season 4 has begun!</b>';
+  announcementDiv.appendChild(heading);
+
+  // Create the paragraph for the main announcement
+  const statsParagraph = document.createElement('p');
+  statsParagraph.style.backgroundColor = 'orange';
+  statsParagraph.innerHTML = '<b>A tonne of stats yet to be created over time. A playoff page, list of Champions etc...</b>';
+
+  // Create the note paragraph with list-style indicators
+  const noteParagraph = document.createElement('p');
+  noteParagraph.innerHTML = `
+    <b>*</b> HaxBall main logo is a link back to home settings of the page. <b>*</b><br>
+    <b>**</b> Tables are sortable via clicking the field header. On mobile, click on any row <b>**</b>
+  `;
+
+  // Append all elements to the container
+  containerDiv.appendChild(announcementDiv);
+  containerDiv.appendChild(statsParagraph);
+  containerDiv.appendChild(noteParagraph);
+
+  // Append the container to the display area
+  displayArea.appendChild(containerDiv);
+
+  // Close the sidebar
+  closeSidebar();
+}
