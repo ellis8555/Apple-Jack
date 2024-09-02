e3469d558bb1947d8eb03a630e515e2b13c3f543
ellis <lsp1@hotmail.com>
2024-09-01 22:54:25 -0600

    close side bar after clicking on menu items

--- src/scripts/listeners/playerTableListeners/playerTableListeners.js
+++ src/scripts/listeners/playerTableListeners/playerTableListeners.js
@@ -1,5 +1,6 @@
 import setPlayersFullTable from "../../tables/setPlayersFullTable";
 import setPlayersAllTimeTable from "../../tables/setPlayersAllTimeTable";
+import closeSidebar from "../../sidebar/closeSidebar";
 
 // Players tables
 // all time player stats
@@ -7,68 +8,107 @@ document
   .getElementById("allTimePlayerPointsTable")
   .addEventListener("click", () => {
     setPlayersAllTimeTable(0, "", "Stats");
+    setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("allTimePlayerSeasonTable")
   .addEventListener("click", () => {
     setPlayersAllTimeTable(0, "Season", "Season Stats");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("allTimePlayerPlayoffTable")
   .addEventListener("click", () => {
     setPlayersAllTimeTable(0, "Playoff", "Playoff Stats");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 04 players tables
 document
   .getElementById("s04PlayerSeasonTable")
   .addEventListener("click", () => {
     setPlayersFullTable();
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 03 player tables
 document
   .getElementById("s03PlayerSeasonTable")
   .addEventListener("click", () => {
     setPlayersFullTable(3);
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s03PlayerPlayoffTable")
   .addEventListener("click", () => {
     setPlayersFullTable(3, "Playoff", "Playoffs");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s03PlayerCombinedTable")
   .addEventListener("click", () => {
     setPlayersFullTable(3, "Combined", "Combined");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 02 player tables
 document
   .getElementById("s02PlayerSeasonTable")
   .addEventListener("click", () => {
     setPlayersFullTable(2);
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s02PlayerPlayoffTable")
   .addEventListener("click", () => {
     setPlayersFullTable(2, "Playoff", "Playoffs");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s02PlayerCombinedTable")
   .addEventListener("click", () => {
     setPlayersFullTable(2, "Combined", "Combined");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 03 player tables
 document
   .getElementById("s01PlayerSeasonTable")
   .addEventListener("click", () => {
     setPlayersFullTable(1);
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s01PlayerPlayoffTable")
   .addEventListener("click", () => {
     setPlayersFullTable(1, "Playoff", "Playoffs");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s01PlayerCombinedTable")
   .addEventListener("click", () => {
     setPlayersFullTable(1, "Combined", "Combined");
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
--- src/scripts/listeners/seasonTableListeners/seasonTableListeners.js
+++ src/scripts/listeners/seasonTableListeners/seasonTableListeners.js
@@ -1,3 +1,4 @@
+import closeSidebar from "../../sidebar/closeSidebar";
 import setSeasonsFullTable from "../../tables/setSeasonsFullTable";
 
 // teams tables
@@ -6,52 +7,82 @@ document
   .getElementById("s04RegularSeason")
   .addEventListener("click", () => {
     setSeasonsFullTable()
+    setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 03 standings tables
 document
   .getElementById("s03RegularSeason")
   .addEventListener("click", () => {
     setSeasonsFullTable(3)
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s03PlayoffTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(3, "Playoff", "Playoffs")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s03CombinedTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(3, "Combined", "Combined")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 02 standings tables
 document
   .getElementById("s02RegularSeason")
   .addEventListener("click", () => {
     setSeasonsFullTable(2)
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s02PlayoffTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(2, "Playoff", "Playoffs")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s02CombinedTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(2, "Combined", "Combined")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 // season 01 standings tables
 document
   .getElementById("s01RegularSeason")
   .addEventListener("click", () => {
     setSeasonsFullTable(1)
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s01PlayoffTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(1, "Playoff", "Playoffs")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
 document
   .getElementById("s01CombinedTable")
   .addEventListener("click", () => {
     setSeasonsFullTable(1, "Combined", "Combined")
+        setTimeout(() => {
+      closeSidebar();
+    }, 50);
 });
