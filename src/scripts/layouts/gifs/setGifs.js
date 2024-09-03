465979eae759ef29529c65c216bb0548361bf0b7
ellis <lsp1@hotmail.com>
2024-09-02 21:36:36 -0600

    import layout componenets

--- src/scripts/layouts/gifs/setGifs.js
+++ src/scripts/layouts/gifs/setGifs.js
@@ -5,9 +5,10 @@
   import getScoreboardDiv from "../../scoreboard/getScoreboardDiv.js";
   import getTablesDiv from "../../tables/getTablesDiv.js";
   import getTeamsGameResults from "../getTeamsGamesResults/getTeamsGamesResults.js";
-  import createTeamCssLogo from "../../misc/createTeamCssLogo.js";
-  import backButton from "../../misc/backButton.js";
   import observeGifs from "./helpers/observeGifs.js"
+  import gifsHeader from "./components/gifsHeader.js";
+  import setGifContainers from "./components/setGifContainers.js"
+  import noGifs from "./components/noGifs.js";
   
   export default function setGifs(e) {
     clearScoreboardDiv();
@@ -24,118 +25,30 @@
     const thisGamesAwayTeam = teamsMAP.get(+thisGamesResult[0].TeamTwo);
     const thisGamesAwayTeamScore = thisGamesResult[0].TeamTwoScore;
     const thisGamesHighlights = Gifs.filter((item) => item.GameID == gameNumber);
+
+    // arguments for methods used below
+    const argsObject = {
+      teamName, thisGifsSeasonNum,  thisGamesHomeTeam, thisGamesAwayTeam ,thisGamesHomeTeamScore, thisGamesAwayTeamScore, thisGamesHighlights
+    }
+
     if (thisGamesHighlights.length > 0) {
-      // begin title for gifs page
-      const gifsHeaderContainer = document.createElement("div");
-      gifsHeaderContainer.classList.add("gifsHeaderContainer");
-  
-      // Back button
-      const backButtonElement = backButton(
-        "gamesGifsBackButton",
-        teamName,
-        thisGifsSeasonNum,
-        "Season",
-        "gifsBackButton"
-      );
-      gifsHeaderContainer.appendChild(backButtonElement);
-  
-      // Home team section
-      const gifsHomeTeam = document.createElement("div");
-      gifsHomeTeam.classList.add("gifsHomeTeam");
-      gifsHomeTeam.innerHTML = createTeamCssLogo.setGifs(
-        thisGamesHomeTeam,
-        thisGifsSeasonNum,
-        "Home"
-      );
-      gifsHeaderContainer.appendChild(gifsHomeTeam);
-  
-      const gifsHomeTeamScore = document.createElement("div");
-      gifsHomeTeamScore.classList.add("gifsHomeTeamScore");
-      gifsHomeTeamScore.textContent = thisGamesHomeTeamScore;
-      gifsHeaderContainer.appendChild(gifsHomeTeamScore);
-  
-      // "vs" section
-      const gifsVsHeader = document.createElement("div");
-      gifsVsHeader.classList.add("gifsVsHeader");
-      gifsVsHeader.textContent = " vs ";
-      gifsHeaderContainer.appendChild(gifsVsHeader);
-  
-      // Away team section
-      const gifsAwayTeam = document.createElement("div");
-      gifsAwayTeam.classList.add("gifsAwayTeam");
-      gifsAwayTeam.innerHTML = createTeamCssLogo.setGifs(
-        thisGamesAwayTeam,
-        thisGifsSeasonNum,
-        "Away"
-      );
-      gifsHeaderContainer.appendChild(gifsAwayTeam);
-  
-      const gifsAwayTeamScore = document.createElement("div");
-      gifsAwayTeamScore.classList.add("gifsAwayTeamScore");
-      gifsAwayTeamScore.textContent = thisGamesAwayTeamScore;
-      gifsHeaderContainer.appendChild(gifsAwayTeamScore);
   
       // Set the gifs header
       tablesDiv.innerHTML = "";
-      tablesDiv.appendChild(gifsHeaderContainer);
+      tablesDiv.appendChild(gifsHeader(argsObject));
   
       // listener for the back button back to teams layout Page
       document
         .getElementById("gamesGifsBackButton")
         .addEventListener("click", getTeamsGameResults);
       // end back button
-      for (let i = 0; i < thisGamesHighlights.length; i++) {
-        let thisGamesFinalPath;
-        const theseGifsSubSet = thisGamesHighlights[0].Filepath;
-        const thisGifsSubPath = theseGifsSubSet.slice(
-          0,
-          theseGifsSubSet.length - 6
-        );
-        if (i < 9) {
-          thisGamesFinalPath = `${thisGifsSubPath}0${i + 1}.gif`;
-        } else {
-          thisGamesFinalPath = `${thisGifsSubPath}${i + 1}.gif`;
-        }
-        const thisGif = thisGamesHighlights.find(
-          (item) => item.Filepath == thisGamesFinalPath
-        );
-              // Create a container for each GIF and comment
-      const gifContainer = document.createElement("div");
-      gifContainer.style.minHeight = "100px";
-      i == 0 ? gifContainer.classList.add("gifContainer", "firstGif") : gifContainer.classList.add("gifContainer", "observedGif")
+      // Append the gifs to the scoreboardDiv
+      scoreboardDiv.appendChild(setGifContainers(argsObject));
 
-      gifContainer.setAttribute('data-imgSrc', thisGamesFinalPath)
-      gifContainer.setAttribute('data-gifComment', thisGif.Comment)
-
-      // Add the comment to the first gif
-      if(i == 0){
-        const gifComment = document.createElement("h5");
-        gifComment.textContent = thisGif.Comment;
-        gifContainer.appendChild(gifComment);
-  
-        // Add the GIF image to the first img
-        const gifImage = document.createElement("img");
-        gifImage.src = thisGamesFinalPath;
-        gifContainer.appendChild(gifImage);
-      } else {
-        // craete empty comment element 
-        const gifComment = document.createElement("h5");
-        gifContainer.append(gifComment)
-        // create empty img element
-        const gifImage = document.createElement("img");
-        gifContainer.append(gifImage)
-      }
-
-      // Append the container to the scoreboardDiv
-      scoreboardDiv.appendChild(gifContainer);
-
-      }
-
+      // watch gif containers for lazy loading
       observeGifs()
 
     } else {
-      const noGifsMessage = document.createElement("h3");
-      noGifsMessage.textContent = "No highlights for this game";
-      scoreboardDiv.appendChild(noGifsMessage);
+      scoreboardDiv.appendChild(noGifs());
     }
   }
