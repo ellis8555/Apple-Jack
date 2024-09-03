dde819d10287ab110571bb13413eed0c2ede6fc3
ellis <lsp1@hotmail.com>
2024-09-02 21:38:18 -0600

    create setgifscontainer.js

---
+++ src/scripts/layouts/gifs/components/setGifContainers.js
@@ -0,0 +1,50 @@
+function setGifContainers({thisGamesHighlights}){
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < thisGamesHighlights.length; i++) {
+    let thisGamesFinalPath;
+    const theseGifsSubSet = thisGamesHighlights[0].Filepath;
+    const thisGifsSubPath = theseGifsSubSet.slice(
+      0,
+      theseGifsSubSet.length - 6
+    );
+    if (i < 9) {
+      thisGamesFinalPath = `${thisGifsSubPath}0${i + 1}.gif`;
+    } else {
+      thisGamesFinalPath = `${thisGifsSubPath}${i + 1}.gif`;
+    }
+    const thisGif = thisGamesHighlights.find(
+      (item) => item.Filepath == thisGamesFinalPath
+    );
+          // Create a container for each GIF and comment
+    const gifContainer = document.createElement("div");
+    gifContainer.style.minHeight = "100px";
+    i == 0 ? gifContainer.classList.add("gifContainer", "firstGif") : gifContainer.classList.add("gifContainer", "observedGif")
+
+    gifContainer.setAttribute('data-imgSrc', thisGamesFinalPath)
+    gifContainer.setAttribute('data-gifComment', thisGif.Comment)
+
+    // Add the comment to the first gif
+    if(i == 0){
+    const gifComment = document.createElement("h5");
+    gifComment.textContent = thisGif.Comment;
+    gifContainer.appendChild(gifComment);
+
+    // Add the GIF image to the first img
+    const gifImage = document.createElement("img");
+    gifImage.src = thisGamesFinalPath;
+    gifContainer.appendChild(gifImage);
+    } else {
+    // create empty container
+    const gifComment = document.createElement("h5");
+    gifContainer.append(gifComment)
+    // create empty img element
+    const gifImage = document.createElement("img");
+    gifContainer.append(gifImage)
+    }
+    fragment.append(gifContainer)
+    }
+    return fragment
+}
+
+export default setGifContainers;
