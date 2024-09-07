d7e649bfa0dda10ae4fc5b33dd14ab298a732052
ellis <lsp1@hotmail.com>
2024-09-06 18:19:51 -0600

    append to thead after the loop

--- src/scripts/layouts/singleGameStats/singleGameStatsComponents/helpers/boxscorePlayersTableHeadersElements.js
+++ src/scripts/layouts/singleGameStats/singleGameStatsComponents/helpers/boxscorePlayersTableHeadersElements.js
@@ -2,20 +2,19 @@ function boxscorePlayersTableHeadersElements(tableHeaders, sortBy){
     const thead = document.createElement('thead')
     const tr = document.createElement('tr')
 
-    thead.append(tr)
-
     for(let i = 0; i < tableHeaders.length; i++){
         const th = document.createElement('th');
         th.setAttribute('data-field-name', tableHeaders[i]);
-
+        
         if(tableHeaders[i] == sortBy){
             th.classList.add("w3-orange")
         }
-
+        
         th.textContent = tableHeaders[i]
         tr.append(th)
     }
-
+    
+    thead.append(tr)
     return thead
 }
 
