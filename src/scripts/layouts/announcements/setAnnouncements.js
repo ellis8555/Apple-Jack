96ea73db44ca969da1432cb9841876e94264ca42
ellis <lsp1@hotmail.com>
2024-09-22 07:19:30 -0600

    import getTablesDiv

--- src/scripts/layouts/announcements/setAnnouncements.js
+++ src/scripts/layouts/announcements/setAnnouncements.js
@@ -1,12 +1,13 @@
 import closeSidebar from "../../sidebar/closeSidebar.js";
 import clearScoreboardDiv from "../../scoreboard/clearScoreboardDiv.js";
+import getTablesDiv from "../../tables/getTablesDiv.js"
 import AnnouncementsPageLayout from "./AnnouncementsPageLayout.js";
 
 function setAnnouncements() {
   clearScoreboardDiv();
   
   // Get the display area where you want to add content
-  let displayArea = document.getElementById("tablesDiv");
+  let displayArea = getTablesDiv();
 
   // Clear any existing content in the display area
   displayArea.innerHTML = '';
