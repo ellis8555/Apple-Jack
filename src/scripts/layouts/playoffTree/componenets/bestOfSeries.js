5a312af1b458406e9724ab4e8d760a93673b3bd7
ellis <lsp1@hotmail.com>
2024-09-23 21:59:09 -0600

    create bestOfSeries

---
+++ src/scripts/layouts/playoffTree/componenets/bestOfSeries.js
@@ -0,0 +1,11 @@
+import teamsColorMAP from "../../../var_lib/maps/teams/teamsColorMAP";
+import teamsNumMAP from "../../../var_lib/maps/teams/teamsNumMAP";
+
+function bestOfSeries(team, semiGamesArray){
+
+const seriesFrag = document.createDocumentFragment()
+
+return seriesFrag
+}
+
+export default bestOfSeries;
