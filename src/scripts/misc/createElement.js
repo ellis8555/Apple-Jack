8d1816c7f8153195c419efe5cff363aebf2763e8
ellis <lsp1@hotmail.com>
2024-09-29 08:22:02 -0600

    createElement.js created

---
+++ src/scripts/misc/createElement.js
@@ -0,0 +1,9 @@
+function createElement(elementType, classes) {
+    const element = document.createElement(elementType);
+    if(classes){
+        element.classList.add(...classes)
+    }
+    return element;
+}
+
+export default createElement
