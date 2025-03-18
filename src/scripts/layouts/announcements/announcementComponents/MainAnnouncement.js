6fa2398b734ad232532de65c3183758e9d7e0364
ellis <lsp1@hotmail.com>
2025-03-18 16:54:58 -0600

    s05 message added

--- src/scripts/layouts/announcements/announcementComponents/MainAnnouncement.js
+++ src/scripts/layouts/announcements/announcementComponents/MainAnnouncement.js
@@ -3,12 +3,12 @@ function MainAnnouncement(){
   const containerElem = document.createElement('div');
   containerElem.style.margin = 'auto';
   containerElem.style.width = '90%';
-  containerElem.style.backgroundColor = 'rgb(0,255,0)';
+  containerElem.style.backgroundColor = 'rgb(0, 217, 255)';
   containerElem.style.color = '#000000';
 
   const headingElem = document.createElement('h5');
   const bold = document.createElement('b')
-  bold.textContent = "Degeneration Hax are your season 4 champions!";
+  bold.textContent = "Season 5 is ready to begin! We have two new players joining us. ComicalFont and Mr T EX. Returning after a long hiatus we have the very creator of this annual tournament in Heinze57.";
   headingElem.append(bold)
 
   containerElem.append(headingElem);
