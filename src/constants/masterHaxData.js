8ab5bdf632120ecf294144625fedc98b020ece96
ellis <lsp1@hotmail.com>
2025-04-09 09:23:11 -0600

    fetch haxData json from s3 bucket

--- src/constants/masterHaxData.js
+++ src/constants/masterHaxData.js
@@ -1,4 +1,4 @@
-// json game data
-import haxBallData from "../json/haxData"
+const fetchHaxData = await fetch("https://hax94-league.s3.us-east-2.amazonaws.com/json/haxData.json")
+const haxBallData = await fetchHaxData.json();
 
 export const { Teams, Players, GameType, TeamPlayers, GameResults, GamePlayerStats, Gifs } = haxBallData
