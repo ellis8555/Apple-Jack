58e2a5c349e73d9e56c078efe639be80d48612a1
ellis <lsp1@hotmail.com>
2025-04-28 21:04:46 -0600

    create setShotsFor.js

---
+++ src/scripts/classFiles/teams/helpers/teams/helpers/setShotsFor.js
@@ -0,0 +1,28 @@
+function setShotsFor({
+    writeToMAP,
+    seasonNumber}){
+    // set for each season
+    if(seasonNumber){
+      //check if any away games were even played
+        if (
+            this[writeToMAP].get(`season${seasonNumber}AwayGames`).length != 0
+          ) {
+            const homeShots = this[writeToMAP]
+              .get(`season${seasonNumber}HomeGames`)
+              .map(item => item.TeamOneShotsOnGoal)
+              .reduce((shotStart, addShot) => +shotStart + +addShot, 0)
+            const awayShots = this[writeToMAP]
+              .get(`season${seasonNumber}AwayGames`)
+              .map(item => item.TeamTwoShotsOnGoal)
+              .reduce((shotStart, addShot) => +shotStart + +addShot, 0)
+            this[writeToMAP].set(
+              `SOG`,
+              homeShots + awayShots
+            );
+          } else {
+            this[writeToMAP].set(`SOG`, 0);
+          }
+    }
+}
+
+export default setShotsFor;
