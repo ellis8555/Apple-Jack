9a44543e6420f4405e5adb43ff933aaa9fefbd68
ellis <lsp1@hotmail.com>
2025-04-28 21:59:25 -0600

    add checks for proper save % formatting

--- src/scripts/classFiles/teams/helpers/teams/helpers/setSavePercentage.js
+++ src/scripts/classFiles/teams/helpers/teams/helpers/setSavePercentage.js
@@ -11,13 +11,26 @@ function setSavePercentage({
               .get(`SA`)
             const goalsAgainst = this[writeToMAP]
               .get(`GA`)
-            let savePercentage = (1 - (goalsAgainst/shotsAgainst)).toFixed(3).slice(1)
+            let savePercentage = (1 - (goalsAgainst/shotsAgainst)).toFixed(3)
+            // check for 1/0 scenarios and adjust
+            if(!isFinite(savePercentage)){
+              savePercentage = "-"
+            }
+            // adjust formatting as long as proper number is returned. example .999
+            if(savePercentage !== "-" && savePercentage !== 1){
+              savePercentage = savePercentage.slice(1)
+            }
+            // adjust formatting for 1.000 results
+            if(savePercentage === 1){
+              savePercentage = "1.000"
+            }
+            // set the result
             this[writeToMAP].set(
               `SV%`,
               savePercentage
             );
           } else {
-            this[writeToMAP].set(`SV%`, ".000");
+            this[writeToMAP].set(`SV%`, "1.000");
           }
     }
 }
