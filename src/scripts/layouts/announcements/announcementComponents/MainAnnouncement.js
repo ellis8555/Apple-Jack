503a2d74aefbd3aa27d44548b80a78922e37c42b
ellis <lsp1@hotmail.com>
2025-06-05 21:40:44 -0600

    scribethonenest champ message

--- src/scripts/layouts/announcements/announcementComponents/MainAnnouncement.js
+++ src/scripts/layouts/announcements/announcementComponents/MainAnnouncement.js
@@ -3,12 +3,12 @@ function MainAnnouncement(){
   const containerElem = document.createElement('div');
   containerElem.style.margin = 'auto';
   containerElem.style.width = '90%';
-  containerElem.style.backgroundColor = 'rgb(0, 217, 255)';
-  containerElem.style.color = '#000000';
+  containerElem.style.backgroundColor = '#004953';
+  containerElem.style.color = '#ffffff';
 
   const headingElem = document.createElement('h5');
   const bold = document.createElement('b')
-  bold.textContent = "Season 5 is ready to begin! We have two new players joining us. ComicalFont and Mr T EX. Returning after a long hiatus we have the very creator of this annual tournament in Heinze57.";
+  bold.textContent = "Season 5 has concluded! Scribethonenest are this seasons champions! Lowest season standings finish to ever win the much coveted prize, The Hax94 Cup!";
   headingElem.append(bold)
 
   containerElem.append(headingElem);
