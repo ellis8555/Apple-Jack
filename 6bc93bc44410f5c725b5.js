edc4601fda48fd757ddba926518df868da36b9f0
ellis <lsp1@hotmail.com>
2025-06-05 21:42:08 -0600

    champions announcement

--- 6bc93bc44410f5c725b5.js
+++ 6bc93bc44410f5c725b5.js
@@ -5891,12 +5891,12 @@ function MainAnnouncement(){
   const containerElem = document.createElement('div');
   containerElem.style.margin = 'auto';
   containerElem.style.width = '90%';
-  containerElem.style.backgroundColor = 'rgb(0, 217, 255)';
-  containerElem.style.color = '#000000';
+  containerElem.style.backgroundColor = '#004953';
+  containerElem.style.color = '#ffffff';
 
   const headingElem = document.createElement('h5');
   const bold = document.createElement('b')
-  bold.textContent = "Season 5 is ready to begin! We have two new players joining us. ComicalFont and Mr T EX. Returning after a long hiatus we have the very creator of this annual tournament in Heinze57.";
+  bold.textContent = "Season 5 has concluded! Scribethonenest are this seasons champions! Lowest season standings finish to ever win the much coveted prize, The Hax94 Cup!";
   headingElem.append(bold)
 
   containerElem.append(headingElem);
@@ -9302,4 +9302,4 @@ module.exports = __webpack_require__.p + "img/teamLogos/S04/trinityNuclearTestHo
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=738eaa2dd02cb067b90c.js.map
+//# sourceMappingURL=6bc93bc44410f5c725b5.js.map
