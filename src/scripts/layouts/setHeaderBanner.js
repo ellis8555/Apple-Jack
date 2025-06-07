3400c038297efc0ee8f4786b12afdaa9a360dc47
ellis <lsp1@hotmail.com>
2025-06-07 10:50:04 -0600

    add images to header banner for season 5 and up

--- src/scripts/layouts/setHeaderBanner.js
+++ src/scripts/layouts/setHeaderBanner.js
@@ -7,7 +7,7 @@ export default function setHeaderBanner(teamName, seasonNumber) {
   if(previousBannerIcon){
     previousBannerIcon.innerHTML = "";
   }
-    // note this is not the champions banner
+  // note this is not the champions banner
   let header = document.getElementById("headerTeamName");
   const insertionDiv = document.createElement("div");
   insertionDiv.style.display = "flex";
@@ -15,25 +15,48 @@ export default function setHeaderBanner(teamName, seasonNumber) {
   insertionDiv.style.alignItems = "center"
   insertionDiv.id = "headerIcon"
 
-  // get teams css logo
-  const teamsCssLogo = setTeamLogoCss(insertionDiv, seasonNumber, undefined, teamName);
-  const grabLogo = teamsCssLogo.firstElementChild.firstElementChild
-  grabLogo.style.height = "6rem";
-  grabLogo.style.width = "6rem";
-  insertionDiv.append(teamsCssLogo)
-  const cssLogoNode = teamsCssLogo.firstElementChild;
-  cssLogoNode.style.height = "6rem";
-  cssLogoNode.style.width = "8rem";
-  const cssLogoTextNode = cssLogoNode.firstElementChild.firstElementChild
-  cssLogoTextNode.style.fontSize = `clamp(${HC_FONT.setHeaderBanner})`;
-  header.prepend(insertionDiv)
-  let headerChildren = header.childNodes;
-  headerChildren[headerChildren.length-1].textContent = teamName;
-  // add champions name here to display trophy icon when champion team selected
-  let trophy = document.querySelector("i");
-  if (teamName == DEFENDING_CHAMPS) {
-    trophy.classList.add("fa-trophy");
-  } else {
-    trophy.classList.remove("fa-trophy");
-  }
+  const seasonNumberAsNumber = parseInt(seasonNumber)
+  // checks for seasons when teams did not have custom team logos and only css logos
+    if(seasonNumberAsNumber < 5){
+      // get teams css logo
+      const teamsCssLogo = setTeamLogoCss(insertionDiv, seasonNumber, undefined, teamName);
+      const grabLogo = teamsCssLogo.firstElementChild.firstElementChild
+      grabLogo.style.height = "6rem";
+      grabLogo.style.width = "6rem";
+      insertionDiv.append(teamsCssLogo)
+      const cssLogoNode = teamsCssLogo.firstElementChild;
+      cssLogoNode.style.height = "6rem";
+      cssLogoNode.style.width = "8rem";
+      const cssLogoTextNode = cssLogoNode.firstElementChild.firstElementChild
+      cssLogoTextNode.style.fontSize = `clamp(${HC_FONT.setHeaderBanner})`;
+      header.prepend(insertionDiv)
+    } else {
+      // seasons where teams now have ai generated team logos
+      const seasonNumberFolderName = seasonNumberAsNumber>9 ? `S${seasonNumber}` : `S0${seasonNumber}`
+      const teamLogo = `../../img/teamLogos/${seasonNumberFolderName}/${teamName}.png`
+
+      const imgElement = document.createElement('img')
+      imgElement.src = teamLogo
+      imgElement.alt = teamName
+      imgElement.style.height = "9rem"
+      imgElement.style.width = "7rem"
+      imgElement.style.objectFit = "contain"
+      imgElement.style.display = "block"
+
+      insertionDiv.append(imgElement)
+      
+      // get teams css logo
+      header.prepend(insertionDiv)
+    }
+    // append the teams name below logo
+    const headerChildren = header.childNodes;
+    headerChildren[headerChildren.length-1].textContent = teamName;
+    // add champions name here to display trophy icon when champion team selected
+    let trophy = document.querySelector("i");
+    if (teamName == DEFENDING_CHAMPS) {
+      trophy.classList.add("fa-trophy");
+    } else {
+      trophy.classList.remove("fa-trophy");
+    }
+
 }
