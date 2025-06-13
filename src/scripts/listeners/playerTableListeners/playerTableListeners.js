12989e618792a8131d3d4029344dce89579a5394
ellis <lsp1@hotmail.com>
2025-06-13 08:22:40 -0600

    listenerers dynamically added team/player sidebar stats

--- src/scripts/listeners/playerTableListeners/playerTableListeners.js
+++ src/scripts/listeners/playerTableListeners/playerTableListeners.js
@@ -2,6 +2,9 @@ import setPlayersFullTable from "../../tables/setPlayersFullTable";
 import setPlayersAllTimeTable from "../../tables/setPlayersAllTimeTable";
 import closeSidebar from "../../sidebar/closeSidebar";
 import seasonCount from "../../var_lib/season/seasonCount";
+import { IN_BETWEEN_SEASONS, IS_PLAYOFFS } from "../../../constants/consts/vars";
+
+const latestSeason = seasonCount.at(-1)
 
 // Players tables
 // all time player stats
@@ -29,6 +32,7 @@ document
       closeSidebar();
     }, 50);
 });
+
 // season 05 players tables
 for(let i = seasonCount.length; i>0; i--){
   document
@@ -39,20 +43,40 @@ for(let i = seasonCount.length; i>0; i--){
       closeSidebar();
     }, 50);
 });
-document
-  .getElementById(`s0${i}PlayerPlayoffTable`)
-  .addEventListener("click", () => {
-    setPlayersFullTable(i, "Playoff", "Playoffs")
-    setTimeout(() => {
-      closeSidebar();
-    }, 50);
-});
-document
-  .getElementById(`s0${i}PlayerCombinedTable`)
-  .addEventListener("click", () => {
-    setPlayersFullTable(i, "Combined", "Combined")
-    setTimeout(() => {
-      closeSidebar();
-    }, 50);
-});
+if(!latestSeason){
+  document
+    .getElementById(`s0${i}PlayerPlayoffTable`)
+    .addEventListener("click", () => {
+      setPlayersFullTable(i, "Playoff", "Playoffs")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+  document
+    .getElementById(`s0${i}PlayerCombinedTable`)
+    .addEventListener("click", () => {
+      setPlayersFullTable(i, "Combined", "Combined")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+}
+if(latestSeason && IN_BETWEEN_SEASONS && IS_PLAYOFFS){
+    document
+    .getElementById(`s0${i}PlayerPlayoffTable`)
+    .addEventListener("click", () => {
+      setPlayersFullTable(i, "Playoff", "Playoffs")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+  document
+    .getElementById(`s0${i}PlayerCombinedTable`)
+    .addEventListener("click", () => {
+      setPlayersFullTable(i, "Combined", "Combined")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+}
 }
