b1904964c80fd0e0c3b379c6864696b325ee7e2b
ellis <lsp1@hotmail.com>
2025-06-13 08:22:40 -0600

    listenerers dynamically added team/player sidebar stats

--- src/scripts/listeners/seasonTableListeners/seasonTableListeners.js
+++ src/scripts/listeners/seasonTableListeners/seasonTableListeners.js
@@ -2,6 +2,9 @@ import playoffTree from "../../layouts/playoffTree/playoffTree";
 import closeSidebar from "../../sidebar/closeSidebar";
 import setSeasonsFullTable from "../../tables/setSeasonsFullTable";
 import seasonCount from "../../var_lib/season/seasonCount";
+import { IN_BETWEEN_SEASONS, IS_PLAYOFFS } from "../../../constants/consts/vars";
+
+const latestSeason = seasonCount.at(-1)
 
 for(let i = seasonCount.length; i>0; i--){
   document
@@ -12,20 +15,40 @@ for(let i = seasonCount.length; i>0; i--){
       closeSidebar();
     }, 50);
 });
-document
-  .getElementById(`s0${i}PlayoffTree`)
-  .addEventListener("click", () => {
-    playoffTree(i)
-    setTimeout(() => {
-      closeSidebar();
-    }, 50);
-});
-document
-  .getElementById(`s0${i}CombinedTable`)
-  .addEventListener("click", () => {
-    setSeasonsFullTable(i, "Combined", "Combined")
-    setTimeout(() => {
-      closeSidebar();
-    }, 50);
-});
+if(!latestSeason){
+  document
+    .getElementById(`s0${i}PlayoffTree`)
+    .addEventListener("click", () => {
+      playoffTree(i)
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+  document
+    .getElementById(`s0${i}CombinedTable`)
+    .addEventListener("click", () => {
+      setSeasonsFullTable(i, "Combined", "Combined")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+}
+if(latestSeason && IN_BETWEEN_SEASONS && IS_PLAYOFFS){
+  document
+    .getElementById(`s0${i}PlayoffTree`)
+    .addEventListener("click", () => {
+      playoffTree(i)
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+  document
+    .getElementById(`s0${i}CombinedTable`)
+    .addEventListener("click", () => {
+      setSeasonsFullTable(i, "Combined", "Combined")
+      setTimeout(() => {
+        closeSidebar();
+      }, 50);
+  });
+}
 }
