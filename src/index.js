522804f3b39a80386f67ce738984d30d996f11cc
ellis <lsp1@hotmail.com>
2025-06-26 10:31:54 -0600

    temp add service worker check for dev mode

--- src/index.js
+++ src/index.js
@@ -17,41 +17,44 @@ import "./scripts/listeners/tablesDivObserver"
 // img files all imported using script
 import images from "./scripts/loadImages";
 import currentSeason from "./scripts/var_lib/season/currentSeason"
-    //////////////////////////////////////////////////
-    // Set homepage standings table for current season
-    //////////////////////////////////////////////////
-    // setHomeTable();
-    ///////////////////
-    // during playoffs
-    ///////////////////
-    // setSeasonsFullTable(5, "Playoff", "Playoffs")
-    playoffTree(currentSeason)
-    // Set listeners on table headers
-    setTableListeners();
-    // service worker for caching begin checking local storage first
+//////////////////////////////////////////////////
+// Set homepage standings table for current season
+//////////////////////////////////////////////////
+// setHomeTable();
+///////////////////
+// during playoffs
+///////////////////
+// setSeasonsFullTable(5, "Playoff", "Playoffs")
+playoffTree(currentSeason)
+// Set listeners on table headers
+setTableListeners();
+// service worker for caching begin checking local storage first
+const useServiceWorker = true
+if(useServiceWorker){
     const getLastModifedJson = await fetch("https://hax94-league.s3.us-east-2.amazonaws.com/json/haxLastModified.json")
     const response = await getLastModifedJson.json()
     const lastModified = response['lastModified']
     let isHaxDataUpdated
     let getLastModifiedHaxData = localStorage.getItem("haxDataLastModified")
     if(getLastModifiedHaxData == null){
-        localStorage.setItem("haxDataLastModified", lastModified)
-        isHaxDataUpdated = false
-    } else {
-        isHaxDataUpdated = lastModified === getLastModifiedHaxData ? true : false
-        if(!isHaxDataUpdated){
             localStorage.setItem("haxDataLastModified", lastModified)
-        }
-    }
-
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js').then(registration => {
-            if(registration.active){
-                registration.active.postMessage({type: "LAST_MODIFIED", payload: isHaxDataUpdated})
-            } else {
-                navigator.serviceWorker.ready.then(registration => {
-                    registration.active.postMessage({type: "LAST_MODIFIED", payload: isHaxDataUpdated})
-                })
+            isHaxDataUpdated = false
+        } else {
+            isHaxDataUpdated = lastModified === getLastModifiedHaxData ? true : false
+            if(!isHaxDataUpdated){
+                localStorage.setItem("haxDataLastModified", lastModified)
             }
-    })
+        }
+    
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./sw.js').then(registration => {
+                if(registration.active){
+                    registration.active.postMessage({type: "LAST_MODIFIED", payload: isHaxDataUpdated})
+                } else {
+                    navigator.serviceWorker.ready.then(registration => {
+                        registration.active.postMessage({type: "LAST_MODIFIED", payload: isHaxDataUpdated})
+                    })
+                }
+            })
+        }
 }
