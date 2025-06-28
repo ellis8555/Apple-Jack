eb0c82a16207b649f553461c1f708847cbd500a7
ellis <lsp1@hotmail.com>
2025-06-28 09:37:31 -0600

    label season games in game results

--- 35d35c397e3fdd4f8c0a.js
+++ 35d35c397e3fdd4f8c0a.js
@@ -2484,9 +2484,14 @@ function getTeamsGameResults(e) {
         "teamsSeason" + seasonNum + gameType + "Stats"
       ][0];
 
-      const firstRoundGamesCount = teamsGames.filter(game => game.Round === 1).length
-      const secondRoundGamesCount = teamsGames.filter(game => game.Round === 2).length
-      const thirdRoundGamesCount = teamsGames.filter(game => game.Round === 3).length
+      let firstRoundGamesCount = 0
+      let secondRoundGamesCount = 0
+      let thirdRoundGamesCount = 0
+      if(gameType === "Playoff"){
+        firstRoundGamesCount = teamsGames.filter(game => game.Round === 1).length
+        secondRoundGamesCount = teamsGames.filter(game => game.Round === 2).length
+        thirdRoundGamesCount = teamsGames.filter(game => game.Round === 3).length
+      }
       
       const gamesLength = teamsGames.length;
       // seasons where teams now have ai generated team logos
@@ -2576,7 +2581,15 @@ function getTeamsGameResults(e) {
       }
 
       if(gamesLength > 0){
+
+        
         for (let i = 0; i < gamesLength; i++) {
+          
+        if(gameType === "Season"){
+          const gameNumber = document.createElement('h6');
+          gameNumber.textContent = `Game ${i+1}`;
+          gameResultsFrag.append(gameNumber)
+        }
 
               // get home and away team names
         let homeTeamName = `${_var_lib_maps_teams_teamsMAP__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .A.get(+teamsGames[i].TeamOne)}`
@@ -10124,4 +10137,4 @@ module.exports = __webpack_require__.p + "img/teamLogos/S05/USHAX.png";
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=e5d563dc5e03039bf9bd.js.map
+//# sourceMappingURL=35d35c397e3fdd4f8c0a.js.map
