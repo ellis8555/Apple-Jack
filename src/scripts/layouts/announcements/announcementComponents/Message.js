fc446963b4b2fa1f3049f4f3c3d7bba114c0ca04
ellis <lsp1@hotmail.com>
2025-06-28 09:00:27 -0600

    add nhl94 banner img

--- src/scripts/layouts/announcements/announcementComponents/Message.js
+++ src/scripts/layouts/announcements/announcementComponents/Message.js
@@ -1,6 +1,6 @@
 function Message(){
     const message = document.createElement('p');
-    message.innerHTML = `Hax94 is a league that began as a tournament between players from the classic title EA NHL 94. <br> NHL 94 league website can be found at <a href="http://www.nhl94.com" style="color:red">here</a>. <br><br> There are also a few players from the NHL 95 community as well.  
+    message.innerHTML = `Hax94 is a league that began as a tournament between players from the classic title EA NHL 94. <br> The NHL 94 community website can be found at <br><a href="http://www.nhl94.com"><img style="max-width:100%;height:auto;" src="../../../../../img/nhl94.com.png" alt="NHL94.com"/> </a> <br><br> There are also a few players from the NHL 95 community as well.  
     `
     return message;
 }
