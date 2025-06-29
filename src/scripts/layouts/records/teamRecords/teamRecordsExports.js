03bdb957a09a30d408af5745031def91cca98069
ellis <lsp1@hotmail.com>
2025-06-29 11:14:06 -0600

    add method setListnersOnFormSelects

--- src/scripts/layouts/records/teamRecords/teamRecordsExports.js
+++ src/scripts/layouts/records/teamRecords/teamRecordsExports.js
@@ -1,11 +1,18 @@
 import teamGoalsScored from "./teamGoalsScored/teamGoalsScored.js";
 import setRecordsPage from "../recordViews/setRecordsPage.js";
+import showSelectedRecords from "../genericRecordFunctions/showSelectedRecords.js";
 
 const leagueTeamRecords = {
   // set records page nav
   setRecordsPageNav: setRecordsPage,
   // teamGoalsScord.js
   teamGoals: teamGoalsScored,
+  setListenersOnFormSelects: function(){
+    const getRecordsFormSelects = document.querySelectorAll('select')
+    getRecordsFormSelects.forEach(select => {
+        select.addEventListener('change', showSelectedRecords)
+    })
+  }
 };
 
 export default leagueTeamRecords;
