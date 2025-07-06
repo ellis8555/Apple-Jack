cee5f623b8f7b059c3842c1c009ae6dd3493765a
ellis <lsp1@hotmail.com>
2025-07-06 09:51:31 -0600

    remove clg

--- src/scripts/tables/createTable.js
+++ src/scripts/tables/createTable.js
@@ -152,7 +152,6 @@ export default function createTable(
             teamLogo.alt = 'img'
             const playersTeamID = TeamPlayers.filter((seasonNum) => seasonNum.SeasonNumber == seasonNumber).filter((player) => player.PlayerID === playersNumMAP.get(item.get('Name')))[0].TeamID
             const playersTeamName = Teams.find((team) => team.TeamID === playersTeamID).TeamName
-            console.log(playersTeamName)
             const teamsLogoName = eachTeamObjectMAP.get(playersTeamName)[`${seasonNumberFolderName}HomeFilePath`]
             teamLogo.src = `../../../img/teamLogos/${seasonNumberFolderName}/${teamsLogoName}.png`
             if((window.innerWidth < TABLE_BREAK_POINT)){
